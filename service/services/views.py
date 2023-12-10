1b9e611555c6dcd141ee88dd04b516327c04b227
Flayzex <redkiyflayzex@gmail.com>
2023-12-10 21:24:53 +0500

    Day 3 - 2

--- service/services/views.py
+++ service/services/views.py
@@ -1,7 +1,7 @@
-from django.db.models import Prefetch
+from django.db.models import F, Sum
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
-from .models import Subscription, Client
+from .models import Subscription
 from .serializers import SubscriptionSerializer
 
 
@@ -10,5 +10,15 @@ class SubscriptionView(ReadOnlyModelViewSet):
         'client__user__email',
         'client__company_name',
         'plan_id',
-    )
+    ).annotate(price=F('service__full_price') - F('service__full_price') * F('plan__discount_percent') / 100.0)
     serializer_class = SubscriptionSerializer
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+        response = super().list(request, *args, **kwargs)
+
+        response_data = {'result': response.data}
+        response_data['total_amount'] = queryset.aggregate(total=Sum('price')).get('total')
+        response.data = response_data
+
+        return response
